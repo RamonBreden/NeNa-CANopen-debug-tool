5ebd05bb9b30d3c644fe30c09763998f84a9392d
Willem van Rossum <wvanrossum9991@gmail.com>
2022-12-23 13:23:57 +0100

    added glow excample

--- MainWindow.py
+++ MainWindow.py
@@ -76,38 +76,8 @@ area.addDock(d7, 'right')
 ## Add widgets into each dock
 
 ## first dock gets save/restore buttons
-def SaveWidget():
-    w1 = pg.LayoutWidget()
-    label = QtWidgets.QLabel(""" -- DockArea Example -- 
-    This window has 6 Dock widgets in it. Each dock can be dragged
-    by its title bar to occupy a different space within the window 
-    but note that one dock has its title bar hidden). Additionally,
-    the borders between docks may be dragged to resize. Docks that are dragged on top
-    of one another are stacked in a tabbed layout. Double-click a dock title
-    bar to place it in its own window.
-    """)
-    saveBtn = QtWidgets.QPushButton('Save dock state')
-    restoreBtn = QtWidgets.QPushButton('Restore dock state')
-    
-    restoreBtn.setEnabled(False)
 
-    w1.addWidget(label, row=0, col=0)
-    w1.addWidget(saveBtn, row=1, col=0)
-    w1.addWidget(restoreBtn, row=2, col=0)
 
-    state = None
-    def save():
-        global state
-        state = area.saveState()
-        restoreBtn.setEnabled(True)
-    saveBtn.clicked.connect(save)
-
-    def load():
-        global state
-        area.restoreState(state)
-    restoreBtn.clicked.connect(load)
-    return w1
-d1.addWidget(SaveWidget())
 
 
 w2 = ConsoleWidget()
@@ -254,19 +224,20 @@ timer.start(15)
 
 
 
-def gen_plotter():
+#def gen_plotter():
    #proberen een nieuw box met een plot te generen
-    win= pg.PlotWidget(title="Dock 6 plot")
-    win.plot(np.random.normal(size=100))
-    d7.addWidget(win)
-    w2.write('new plot (NOT YET WORKING) kun jij die doen Ramon\n' , scrollToBottom='auto')
+    #win= pg.PlotWidget(title="Dock 6 plot")
+    #win.plot(np.random.normal(size=100))
+    #d7.addWidget(win)
+   # w2.write('new plot (NOT YET WORKING) kun jij die doen Ramon\n' , scrollToBottom='auto')
     
    #PlotterWindow.Upload_Window()
 #GenPLotButton.clicked.connect(gen_plotter)
 d4.addWidget(w4)
 
+
 #make the list of nodes on the canbus, with dropdown list of the objects
-w6 =pg.mkQApp()
+#w6 =pg.mkQApp()
 w6 = pg.TreeWidget()
 node_list = [41, 42, 10]
 ObjectlistNode1= [46546, 6454, 4]
@@ -284,6 +255,102 @@ for i in range(len(node_list)):
 
 d6.addWidget(w6)
 
+
+
+pg.setConfigOptions(antialias=True)
+
+
+# Dedicated colors which look "good"
+colors = ['#08F7FE', '#FE53BB', '#F5D300', '#00ff41', '#FF0000', '#9467bd', ]
+
+# We create the ParameterTree
+children = [
+    dict(name='make_line_glow', type='bool', value=False),
+    dict(name='add_underglow', type='list', limits=['None', 'Full', 'Gradient'], value='None'),
+    dict(name='nb_lines', type='int', limits=[1, 6], value=1),
+    dict(name='nb glow lines', type='int', limits=[0, 15], value=10),
+    dict(name='alpha_start', type='int', limits=[0, 255], value=25, step=1),
+    dict(name='alpha_stop', type='int', limits=[0, 255], value=25, step=1),
+    dict(name='alpha_underglow', type='int', limits=[0, 255], value=25, step=1),
+    dict(name='linewidth_start', type='float', limits=[0.1, 50], value=1, step=0.1),
+    dict(name='linewidth_stop', type='float', limits=[0.2, 50], value=8, step=0.1),
+]
+
+params = pg.parametertree.Parameter.create(name='Parameters', type='group', children=children)
+pt = pg.parametertree.ParameterTree(showHeader=False)
+pt.setParameters(params)
+
+
+pw2 = pg.PlotWidget()
+splitter = QtWidgets.QSplitter()
+splitter.addWidget(pt)
+splitter.addWidget(pw2)
+splitter.show()
+
+# Add some noise on the curves
+noise  = 0.1
+noises: list = np.random.rand(6, 100)*noise
+
+def update_plot():
+    
+    pw2.clear()
+
+    nb_glow_lines   = params.child('nb glow lines').value()
+    alpha_start     = params.child('alpha_start').value()
+    alpha_stop      = params.child('alpha_stop').value()
+    alpha_underglow = params.child('alpha_underglow').value()
+    linewidth_start = params.child('linewidth_start').value()
+    linewidth_stop  = params.child('linewidth_stop').value()
+    nb_lines        = params.child('nb_lines').value()
+
+    xs = []
+    ys = []
+    for i in range(nb_lines):
+        xs.append(np.linspace(0, 2*np.pi, 100)-i)
+        ys.append(np.sin(xs[-1])*xs[-1]-i/3+noises[i])
+
+    # For each line we:
+    # 1. Add a PlotDataItem with the pen and brush corresponding to the line
+    #    color and the underglow
+    # 2. Add nb_glow_lines PlotDatamItem with increasing width and low alpha
+    #    to create the glow effect
+    for color, x, y in zip(colors, xs, ys):
+        pen = pg.mkPen(color=color)
+        if params.child('add_underglow').value()=='Full':
+            kw={'fillLevel' : 0.0,
+                'fillBrush' : pg.mkBrush(color='{}{:02x}'.format(color, alpha_underglow)),
+                }
+        elif params.child('add_underglow').value()=='Gradient':
+            grad = QtGui.QLinearGradient(x.mean(), y.min(), x.mean(), y.max())
+            grad.setColorAt(0.001, pg.mkColor(color))
+            grad.setColorAt(abs(y.min())/(y.max()-y.min()), pg.mkColor('{}{:02x}'.format(color, alpha_underglow)))
+            grad.setColorAt(0.999, pg.mkColor(color))
+            brush = QtGui.QBrush(grad)
+            kw={'fillLevel' : 0.0,
+                'fillBrush' : brush,
+                }
+        else:
+            kw = {}
+        pw2.addItem(pg.PlotDataItem(x, y, pen=pen, **kw))
+
+
+        if params.child('make_line_glow').value():
+            alphas = np.linspace(alpha_start, alpha_stop, nb_glow_lines, dtype=int)
+            lws = np.linspace(linewidth_start, linewidth_stop, nb_glow_lines)
+
+            for alpha, lw in zip(alphas, lws):
+
+                pen = pg.mkPen(color='{}{:02x}'.format(color, alpha),
+                               width=lw,
+                               connect="finite")
+
+                pw2.addItem(pg.PlotDataItem(x, y,
+                                            pen=pen))
+
+params.sigTreeStateChanged.connect(update_plot)
+update_plot()
+d7.addWidget(pw2)
+d1.addWidget(pt)
 win.show()
 
 if __name__ == '__main__':
